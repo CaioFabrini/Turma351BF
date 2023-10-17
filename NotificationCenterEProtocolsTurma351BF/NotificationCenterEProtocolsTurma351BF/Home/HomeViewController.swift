c81f1fcd82147f06b4f128864f75ae7bd0af4b36
Caio Fabrini <caiofabrini@outlook.com>
2023-10-17 20:22:23 -0300

    aprendendo sobre protocol

--- NotificationCenterEProtocolsTurma351BF/NotificationCenterEProtocolsTurma351BF/Home/HomeViewController.swift
+++ NotificationCenterEProtocolsTurma351BF/NotificationCenterEProtocolsTurma351BF/Home/HomeViewController.swift
@@ -21,27 +21,40 @@ class HomeViewController: UIViewController {
     
     func configObserver() {
         NotificationCenter.default.addObserver(self, selector: #selector(changeMacbook), name: .macbook, object: nil)
-        
         NotificationCenter.default.addObserver(self, selector: #selector(changeImac), name: .imac, object: nil)
     }
     
     @objc func changeMacbook(_ notification: NSNotification) {
 //        let color = notification.object as? String
         view.backgroundColor = .red
+        logoImageView.image = UIImage(named: "macbook_pro")
+        titleLabel.text = "Macbook Pro"
     }
     
     @objc func changeImac(_ notification: NSNotification) {
 //        let color = notification.object as? String
-        view.backgroundColor = .purple
+        view.backgroundColor = .cyan
+        logoImageView.image = UIImage(named: "imac_pro")
+        titleLabel.text = "Imac das galáxias"
     }
     
     
     @IBAction func tappedChooseButton(_ sender: UIButton) {
         let vc = UIStoryboard(name: "OptionsViewController", bundle: nil).instantiateViewController(withIdentifier: "OptionsViewController") as? OptionsViewController
         vc?.modalPresentationStyle = .fullScreen
+        vc?.delegate = self
         present(vc ?? UIViewController(), animated: true)
     }
     
+}
 
+extension HomeViewController: OptionsViewControllerProtocol {
+    func actionMacbook() {
+        view.backgroundColor = .orange
+    }
+    
+    func actionImac() {
+        view.backgroundColor = .purple
+    }
 }
 
--- NotificationCenterEProtocolsTurma351BF/NotificationCenterEProtocolsTurma351BF/Options/OptionsViewController.swift
+++ NotificationCenterEProtocolsTurma351BF/NotificationCenterEProtocolsTurma351BF/Options/OptionsViewController.swift
@@ -7,27 +7,61 @@
 
 import UIKit
 
+// Como criar um protocol?
+// Utilize a palavra reservada protocol
+// Na sequencia o nome do protocol
+// Crie o protocol do lado DE FORA da classe
+// Coloque o protocol para ser do tipo AnyObject
+// Crie as propriedades/metodos no protocol
+// Dentro da classe crie um delegate
+
+
+// Para trabalhar com protocol e não ter dor de cabeça
+
+// Crie o protocol
+// Utilize o protocol Ex: delegate?.tappedButton()
+// ASSINAR O PROTOCOL Ex: viewController?.delegate = self (SE CASO NÃO ASSINAR, JAMAIS IRÁ FUNCIONAR)
+
+
+
+
+// O nome é o nome da classe + o final "Protocol"
+protocol OptionsViewControllerProtocol: AnyObject {
+    func actionMacbook()
+    func actionImac()
+}
+
 class OptionsViewController: UIViewController {
     
     @IBOutlet weak var macbookButton: UIButton!
     @IBOutlet weak var imacButton: UIButton!
     
+    weak var delegate: OptionsViewControllerProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     @IBAction func tappedMacbookButton(_ sender: Any) {
         // Neste exemplo alem de gritar a nossa chave "macbook", tambem estamos levando o object UIColor (você pode setar qualquer tipagem no object)
-//        NotificationCenter.default.post(name: NSNotification.Name("macbook"), object: UIColor.cyan)
+        //        NotificationCenter.default.post(name: NSNotification.Name("macbook"), object: UIColor.cyan)
         
-        NotificationCenter.default.post(name: .macbook, object: nil)
+        // Para utilizar notification, descomente a linha abaixo e comente a linha do delegate. Mesma coisa para o inverso
+        // NotificationCenter.default.post(name: .macbook, object: nil)
+        
+        // Protocol
+        delegate?.actionMacbook()
         dismiss(animated: true)
     }
     
     @IBAction func tappedImacButton(_ sender: UIButton) {
-        NotificationCenter.default.post(name: .imac, object: nil)
+        // Para utilizar notification, descomente a linha abaixo e comente a linha do delegate. Mesma coisa para o inverso
+        // NotificationCenter.default.post(name: .imac, object: nil)
+        
+        // Protocol
+        delegate?.actionImac()
         dismiss(animated: true)
     }
-
+    
 }
